2d47f91fff05a3cbf15dc47ec630cd6b8d0ef127
Bruno Fernandes <brunof900@gmail.com>
2023-08-18 02:25:41 -0300

    Threads paralelas

--- curso_projetojava/src/cursojava/thread/AulaThread.java
+++ curso_projetojava/src/cursojava/thread/AulaThread.java
@@ -7,26 +7,53 @@ public class AulaThread {
 	public static void main(String[] args)  throws InterruptedException{
 		// TODO Auto-generated method stub
 		
-		new Thread() {
-			public void run() {
-				for(int pos = 0; pos < 10; pos++) {	
-					/**/
-					System.out.println("Executando alguma rotina");
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-		}.start();
+		Thread threadEmail = new Thread(thread1);
+		threadEmail.start();
+		
+		Thread threadNFCE = new Thread(thread2);
+		threadNFCE.start();
 		
 		System.out.println("Alguma coisa fora da thread");
-		JOptionPane.showMessageDialog(null, "Rodando fora da rotina da thread");
+		JOptionPane.showMessageDialog(null, "Rodando fora da rotina da thread");		
 		
-		
-
 	}
+	
+	 private static Runnable thread1 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for(int pos = 0; pos < 10; pos++) {	
+				/**/
+				System.out.println("Executando email de uma crontab para diferenciação");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	private static Runnable thread2 = new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			for(int pos = 0; pos < 10; pos++) {	
+				/**/
+				System.out.println("Executando uma rotina para nota fiscal");
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
 
 }
