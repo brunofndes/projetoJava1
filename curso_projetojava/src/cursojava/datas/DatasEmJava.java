ae8c99aa515716dd4415f55c42f99cefb3ae910d
Bruno Fernandes <brunof900@gmail.com>
2023-08-20 02:59:18 -0300

    Trabalhando com datas

--- curso_projetojava/src/cursojava/datas/DatasEmJava.java
+++ curso_projetojava/src/cursojava/datas/DatasEmJava.java
@@ -1,22 +1,28 @@
 package cursojava.datas;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		Calendar calendario = Calendar.getInstance();
-		Date date = new Date();
 		
-		System.out.println(date.getDate());
-		System.out.println("Data calendar " + calendario.get(Calendar.DATE));
-		System.out.println("Data calendar " + calendario.get(Calendar.DAY_OF_WEEK));
 		
-		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		
-		System.out.println("Calendar fata atual formatada " + simpleDateFormat.format(calendario.getTime()));
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Date dataVencimentoBoleto = simpleDateFormat.parse("18/08/2023");
+		
+		Date dataAtual = simpleDateFormat.parse("20/08/2023");
+		
+		if(dataVencimentoBoleto.after(dataAtual)) {
+			System.out.println("Boleto não vencido");
+		}else {
+			System.out.println("BOLETO VENCIDO");
+		}
 	}
 
 }
