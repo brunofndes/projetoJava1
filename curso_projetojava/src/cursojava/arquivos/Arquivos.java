e0f698136d7993cc5d71d052afbb68dedc619d77
Bruno Fernandes <brunof900@gmail.com>
2023-08-21 02:09:07 -0300

    Trabalhando com arquivos em Java

---
+++ curso_projetojava/src/cursojava/arquivos/Arquivos.java
@@ -0,0 +1,48 @@
+package cursojava.arquivos;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Arquivos {
+
+	public static void main(String[] args) throws IOException {
+		Pessoas pessoa1 = new Pessoas();
+		pessoa1.setNome("Bruno Fernandes");
+		pessoa1.setEmail("bruno@gmail.com");
+		pessoa1.setIdade(28);
+		
+		Pessoas pessoa2 = new Pessoas();
+		pessoa2.setNome("Lucas Fernandes");
+		pessoa2.setEmail("lucas@gmail.com");
+		pessoa2.setIdade(29);
+		
+		Pessoas pessoa3 = new Pessoas();
+		pessoa3.setNome("Patricia Fernandes");
+		pessoa3.setEmail("patricia@gmail.com");
+		pessoa3.setIdade(20);
+		
+		List<Pessoas> pessoaList = new ArrayList<Pessoas>();
+		pessoaList.add(pessoa1);
+		pessoaList.add(pessoa2);
+		pessoaList.add(pessoa3);
+		
+		File arquivo = new File("C:\\Users\\bruno\\git\\repository\\curso_projetojava\\src\\cursojava\\arquivos\\arquivo.txt");
+		
+		if(!arquivo.exists()) {
+			arquivo.createNewFile();
+		}
+		
+		FileWriter escrever_no_arquivo = new FileWriter(arquivo);
+		
+		for (Pessoas p : pessoaList) {
+			escrever_no_arquivo.write(p.getNome() + " " + p.getEmail() + " " + p.getIdade() + "\n");
+		}
+		
+		escrever_no_arquivo.flush();
+		escrever_no_arquivo.close();
+	}
+
+}
---
+++ curso_projetojava/src/cursojava/arquivos/Pessoas.java
@@ -0,0 +1,26 @@
+package cursojava.arquivos;
+
+public class Pessoas {
+	private String email;
+	private String nome;
+	private int idade;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
